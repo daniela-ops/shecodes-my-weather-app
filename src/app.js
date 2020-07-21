4fb5880075da6fc73b7d666465527f10f19e9042
daniela-ops <66274413+daniela-ops@users.noreply.github.com>
2020-07-21 16:37:09 -0400

    Created a function for humidity and wind

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "b3b730c3e6af10dda8a23714d4b1cfe9";
