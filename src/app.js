072b6bda7f1050314fd5d5935b2c550bfa44708d
daniela-ops <66274413+daniela-ops@users.noreply.github.com>
2020-07-21 16:19:26 -0400

    created a fuction to display temp

--- src/app.js
+++ src/app.js
@@ -1,4 +1,16 @@
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+}
+
 let apiKey = "b3b730c3e6af10dda8a23714d4b1cfe9";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
