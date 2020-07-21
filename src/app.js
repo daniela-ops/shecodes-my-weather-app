a870a4e28676378ad23ef624d2908b30c6fef7a3
daniela-ops <66274413+daniela-ops@users.noreply.github.com>
2020-07-21 15:10:58 -0400

    Tweaked API Key

--- src/app.js
+++ src/app.js
@@ -1,1 +1,4 @@
-let apiKey = "9904e091894bf7a653dc5750f255e4b2";
+let apiKey = "b3b730c3e6af10dda8a23714d4b1cfe9";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
